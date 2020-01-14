0b680f8d8d1d46753192374c1b587925681b73e2
sskywalker <hkrai39@gmail.com>
2020-01-14 18:25:15 +0530

    completed update contact functionality

--- routes/index.js
+++ routes/index.js
@@ -43,4 +43,35 @@ router.get("/delete/:id", (req, res) => {
     .catch(err => res.send(`Opps error occurred at ${err}`));
 });
 
+//update route- Get
+
+router.get("/update/:id", (req, res) => {
+  phoneBookModel
+    .findOne({ _id: req.params.id })
+    .then(contact => {
+      res.render("updatePage", { contact });
+    })
+    .catch(err => res.send(`Opps error occurred at ${err}`));
+});
+
+// post route to update the contact data
+router.post("/update/:id", (req, res) => {
+  const updatedContact = {
+    contact: {
+      name: req.body.contactName,
+      number: req.body.contactNumber
+    }
+  };
+  phoneBookModel
+    .findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: updatedContact },
+      { new: true }
+    )
+    .then(contact => {
+      res.redirect("/viewAll");
+    })
+    .catch(err => res.send(`Opps error occurred at ${err}`));
+});
+
 module.exports = router;
